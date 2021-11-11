5eddb1b35e659004bca10d97e8298129eff7134d
Angelos Anagnostopoulos <angelosanag@protonmail.com>
2021-11-11 17:32:37 +0200

    Added threads to wikipedia scrapper and rewrote README.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # web-scraping-projects
 
-A series of small scripts using requests and bs4 to extract and modify information on a number of ways.
+<p>A series of small scripts using requests and bs4 to extract and modify information on a number of ways.
+
+##wikipediaScrapper.py usage:
+
+<p>python3 wikipediaScrapper.py <wikipedia article name>
--- wikipediaScraper.py
+++ wikipediaScraper.py
@@ -2,6 +2,7 @@
 # wikipediaScraper - Downloads all images in a specified wikipedia page
 
 import sys,os,random,pprint,requests
+import concurrent.futures
 from bs4 import BeautifulSoup
 
 argument = str(sys.argv[1:]).split("'")[1]
@@ -15,29 +16,37 @@ def scrapeWikiArticle(url):
     soup = BeautifulSoup(response.content, 'lxml')
     title = soup.find(id="firstHeading")
     print(title.text)
-    images = {}
+    images = []
     ind = 0
+    
+    #Get all images from the page
     for image in soup.find_all("img"):
 
         image_link = image["src"]
         
         if image_link[0:5] != "https:":
             image_link = "https:" + image["src"]
-        image_name = image_link.split("/")[-1].split("-")[-1]
         
-        if image_name not in images:    
-            images[image_link] = image_name
-        
-        try:
-            res = requests.get(image_link)
-            res.raise_for_status()
-        except Exception as e:
-            pass
+        images.append(image_link)
 
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(downloadImage, images)
+        
+
+def downloadImage(image_link):
+    
+    image_name = image_link.split("/")[-1].split("-")[-1]
+    
+    try:
+        res = requests.get(image_link)
+        res.raise_for_status()
         image_file = open(os.path.join(arg,os.path.basename(image_name)), 'wb')
         for chunk in res.iter_content(100000):
             image_file.write(chunk)
 
         image_file.close()
 
+    except Exception as e:
+        pass
+           
 scrapeWikiArticle(url)
